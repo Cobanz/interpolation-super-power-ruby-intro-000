9412fe090bafdcb4a2f8c96888661bec06c6f9a6
Charles Grant Nichols <grant@inkdots.com>
2021-01-27 05:17:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-  puts R[0]
-  puts O[1]
-  puts Y[2]
-  puts G[3]
-  puts B[4]
-  puts I[5]
-  puts V[6]
+  puts R:[0]
+  puts O:[1]
+  puts Y:[2]
+  puts G:[3]
+  puts B:[4]
+  puts I:[5]
+  puts V:[6]
 end
